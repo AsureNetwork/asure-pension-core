fc1c34ee8bc55e5c84ad1e69b0996a26c4f13db5
Fabian Raetz <fabian.raetz@gmail.com>
2019-01-25 16:08:25 +0100

    use simulator infrastructure in sim1

--- examples/sim1.rs
+++ examples/sim1.rs
@@ -1,65 +1,36 @@
 //cargo run --example sim1
 use asure_pension_core::*;
+use asure_pension_core::user::User;
+
+struct Simulation1;
+
+impl Simulation1 {
+    pub fn new() -> Self {
+        Simulation1 {}
+    }
+}
+
+impl PensionSimulation for Simulation1 {
+    fn name(&mut self) -> String {
+        "Simulation1".to_string()
+    }
+
+    fn create_user(&mut self, current_period: u64) -> u32 {
+        match current_period {
+            1 => 10,
+            _ => 0,
+        }
+    }
+
+    fn should_retire(&mut self, contributor: &User) -> bool {
+        contributor.transactions.len() == 480
+    }
+
+    fn pay_pension(&mut self, _contributor: &User) -> Option<f64> {
+        Some(1.0)
+    }
+}
 
 fn main() {
-    println!("Pension Sim1");
-
-    let mut pension = Pension::new();
-    pension.create_users(10);
-
-    let payment_months = 40 * 12; // 40 years
-    let generations = 8; // number of generations that pay into the systems
-    let total_months = payment_months * generations;
-
-    for i in 0..total_months {
-        // until the nth (2) generation
-        if i < 2 * payment_months {
-            // add n (2) new users every year
-            if i % 12 == 0 {
-                pension.create_users(2);
-            }
-
-            // retire user every two years
-            if i % 24 == 0 {
-                let mut counter = 0;
-                for user in &mut pension.users {
-                    if user.activate_retirement() {
-                        counter = counter + 1;
-                    }
-
-                    if counter == 1 {
-                        break;
-                    }
-                }
-            }
-        }
-
-        pension.start();
-        let mut sum: f64 = 0.0;
-        for user in &mut pension.users {
-            match user.pay(pension.current_period, 20.0) {
-                Ok(()) => sum += 20.0,
-                Err(_) => {}
-            }
-        }
-        pension.add_amount(sum);
-
-        pension.payout();
-        pension.end();
-
-        for user in &pension.users
-            {
-                //Console.WriteLine(
-
-                //    string.Format("{0,10}",user.Name)+" "+
-                //    string.Format("{0,20}", user.Total)+ "  :  "+
-
-                //    (user.PensionPaymentMonths+ " = "+user.PensionRecivedMonths+" / "+user.PensionReciveMonths));
-                println!("User: {} - {} {} DPT: {}", user.id, user.wallet.eth, user.wallet.pension_eth, user.wallet.dpt.amount);
-//            Console.WriteLine(
-//                user.Name + "," +
-//                    user.Total + "," +
-//                    (user.PensionPaymentMonths + "," + user.PensionRecivedMonths + "," + user.PensionReciveMonths));
-            }
-    }
+    Pension::simulate(Simulation1::new());
 }
