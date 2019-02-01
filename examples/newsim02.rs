23e6621702c6621a4d9e9f4a1aea01c909a9b126
Fabian Raetz <fabian.raetz@gmail.com>
2019-02-01 15:43:39 +0100

    port sim02

---
+++ examples/newsim02.rs
@@ -0,0 +1,24 @@
+use asure_pension_core::new::*;
+use asure_pension_core::new::contributor::Contributor;
+use asure_pension_core::new::types::*;
+
+struct Sim;
+
+impl Sim {
+    pub fn new() -> Self {
+        Sim {}
+    }
+}
+
+impl PensionSimulation for Sim {
+    fn should_contribute(&mut self, contributor: &Contributor, _period: Period) -> Option<Unit> {
+        match contributor.id() {
+            0 => Some(2.0),
+            _ => Some(1.0),
+        }
+    }
+}
+
+fn main() {
+    simulate(Sim::new()).unwrap();
+}
