0c65ae7205b26686ab8037247091791711836c22
pmizel <foto@enivo.de>
2019-02-04 23:58:46 +0100

    add fmt output

--- src/new/mod.rs
+++ src/new/mod.rs
@@ -203,23 +203,23 @@ pub fn print(pension: &Pension, users: &[User]) {
                     Some(dpt) => format!("{}", dpt),
                     None => "0".to_string()
                 };
-
-                println!("User: {}, Status: {}, Wallet: {}, Pension: {}, Pension Months Allowed: {}, Pensions Months Received: {}, DPT: {} + ({})",
+                println!("User: {:2}, Status: {}, Wallet: {}, Pension: {:16.12}, Pension Months Allowed: {:3}, Pensions Months Received: {:3}, AVG: ({:10.8}), DPT: {:14.10} + ({})",
                          contributor.id(), "Contributor", contributor.wallet(), 0, contributor.allowed_pension_periods(),
-                         0, contributor.dpt_total(), last_dpt);
+                         0, 0, contributor.dpt_total(), last_dpt);
             }
             User::Pensioner(pensioner) => {
                 let contributor = &pensioner.contributor;
-                println!("User: {}, Status: {}, Wallet: {}, Pension: {}, Pension Months Allowed: {}, Pensions Months Received: {}, DPT: {}",
+                println!("User: {:2}, Status: {}, Wallet: {}, Pension: {:16.12}, Pension Months Allowed: {:3}, Pensions Months Received: {:3}, AVG: ({:10.8}), DPT: {:14.10}",
                          contributor.id(), "Pensioner", contributor.wallet(), pensioner.total_pension(),
-                         contributor.allowed_pension_periods(), pensioner.pension_periods(), contributor.dpt_total());
+                         contributor.allowed_pension_periods(), pensioner.pension_periods(), 0, contributor.dpt_total());
             }
             User::Done(done_user) => {
                 let pensioner = &done_user.pensioner;
                 let contributor = &pensioner.contributor;
-                println!("User: {}, Status: {}, Wallet: {}, Pension: {}, Pension Months Allowed: {}, Pensions Months Received: {}, DPT: {}",
+                let avg = pensioner.total_pension() / pensioner.pension_periods() as f64;
+                println!("User: {:2}, Status: {}, Wallet: {}, Pension: {:16.12}, Pension Months Allowed: {:3}, Pensions Months Received: {:3}, AVG: ({:10.8}), DPT: {:14.10}",
                          contributor.id(), "Done", contributor.wallet(), pensioner.total_pension(),
-                         contributor.allowed_pension_periods(), pensioner.pension_periods(), contributor.dpt_total());
+                         contributor.allowed_pension_periods(), pensioner.pension_periods(), avg, contributor.dpt_total());
             }
         }
     }
