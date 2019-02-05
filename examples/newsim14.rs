1ebe53db7ae03653f5e3aa33c97cbe0395da63da
Fabian Raetz <fabian.raetz@gmail.com>
2019-02-05 12:10:13 +0100

    add another simulation

---
+++ examples/newsim14.rs
@@ -0,0 +1,34 @@
+//cargo run --example newsim05
+use asure_pension_core::new::*;
+use asure_pension_core::new::contributor::*;
+use asure_pension_core::new::types::*;
+use asure_pension_core::new::pensioner::Pensioner;
+
+struct Sim;
+
+impl Sim {
+    pub fn new() -> Self {
+        Sim {}
+    }
+}
+
+impl PensionSimulation for Sim {
+    fn new_contributors(&mut self, current_period: Period) -> u64 {
+        match current_period {
+            1 => 1,
+            960 => 10,
+            _ => 0,
+        }
+    }
+
+    fn should_claim_pension(&mut self, pensioner: &Pensioner, period: Period) -> bool {
+        match pensioner.contributor.id() {
+            0 => period <= 940 || period >= 2470,
+            _ => period >= 2000
+        }
+    }
+}
+
+fn main() {
+    simulate(Sim::new()).unwrap();
+}
